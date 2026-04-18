9c43585d985814335a5f89e3d75bfcb9940c96f3
Aqiba Abdul Qadir <139615591+AqibaAbdulQadir@users.noreply.github.com>
2026-04-19 00:43:05 +0500

    Add top module for UART with controller and transmitter

---
+++ SHAKE_FYP/UART/top.sv
@@ -0,0 +1,59 @@
+module top #(
+    parameter clkdiv = 868,
+    parameter OUTPUT_BUFFER_DEPTH_WORDS = 131072
+)(
+    input  logic clk,
+    input  logic rst_n,
+    input  logic start,
+    
+    output logic tr_start,
+    output logic tr_end,
+    output logic tx
+);
+
+    logic rd_en;
+    logic [7:0] rd_data;
+    logic empty;
+
+    logic tx_start, tx_done;
+    logic [7:0] tx_data;
+    logic [$clog2(OUTPUT_BUFFER_DEPTH_WORDS * (w/8))-1:0] rd_addr;
+
+ shake_top #(.OUTPUT_BUFFER_DEPTH_WORDS(OUTPUT_BUFFER_DEPTH_WORDS))
+    shake (
+    .clk(clk),
+    .rst_n(rst_n),
+    .start(start),
+    .done(tr_start),
+
+    .buffer_dout_byte(rd_data),
+    .buffer_read_en(rd_en),
+    .buffer_read_addr(rd_addr)
+);
+
+    // Controller
+    uart_controller  #(.OUTPUT_BUFFER_DEPTH_WORDS(OUTPUT_BUFFER_DEPTH_WORDS))
+        ctrl (
+        .clk(clk),
+        .n_rst(rst_n),
+        .done(tr_start),
+        .buffer_data(rd_data),
+        .rd_addr(rd_addr),
+        .rd_en(rd_en),
+        .tx_start(tx_start),
+        .tx_data(tx_data),
+        .tx_done(tx_done),
+        .finish(tr_end)
+    );
+
+    // Transmitter
+    transmitter #(.clkdiv(clkdiv)) tx_inst (
+        .clk(clk),
+        .rst(rst_n),
+        .tx_start(tx_start),
+        .tx_data(tx_data),
+        .tx_done(tx_done),
+        .tx(tx)
+    );
+
+endmodule
